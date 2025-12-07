88236658f80b9a0416a7f0ef7c8fc54fdd5b67e9
cubman3134 <cubman3134@gmail.com>
2025-12-07 17:04:57 -0600

    ereader changes

---
+++ Assets/ebook/Scripts/EbookRenderer.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UEPub;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows;
+using static UnityEngine.InputSystem.InputControlScheme.MatchResult;
+
+public enum Tags
+{
+	Unassigned,
+	H1,
+	H2,
+	H3,
+	H4,
+	H5,
+	H6,
+	Paragraph,
+	Head,
+	Title,
+	Emphasis,
+	Bold,
+	ListItem, // li, inside of an ol or ul tag
+	UnorderedList, // ul bullet points rather than numbered
+	OrderedList, // ol 1. 2. 3.
+	Break
+}
+
+public class TaggedData
+{
+	public static Dictionary<string, Tags> TagNamesToTags { get; set; } = new Dictionary<string, Tags>()
+	{
+		{ "h1",		Tags.H1			},
+		{ "h2",		Tags.H2			},
+		{ "h3",		Tags.H3			},
+		{ "h4",		Tags.H4			},
+		{ "h5",		Tags.H5			},
+		{ "h6",		Tags.H6			},
+		{ "p",		Tags.Paragraph	},
+		{ "head",	Tags.Head		},
+		{ "title",	Tags.Title		},
+		{ "em",		Tags.Emphasis	},
+		{ "b",		Tags.Bold		},
+		{ "li",		Tags.ListItem	},
+		{ "ul",		Tags.UnorderedList	},
+		{ "ol",		Tags.OrderedList	},
+		{ "br",		Tags.Break		},
+	};
+
+	public Tags Tag { get; set; }
+
+}
+
+public class EbookRenderer : MonoBehaviour {
+	public Text displayText;
+	public TextAsset testBook;
+
+	//private EpubBook epubBook;
+
+	// Use this for initialization
+	void Start () {
+		OpenEbookFile ();
+	}
+
+	string GetTextWithTags(string text, string tag, bool keepInnerInfo)
+	{
+        System.Text.RegularExpressions.Match match = null;
+        do
+        {
+            match = Regex.Match(text, $"<{tag}.*?>(.*?)<\\/{tag}>");
+            if (!match.Success)
+            {
+                break;
+            }
+            var tagLength = match.Groups[1].Index - match.Groups[0].Index;
+            var preText = text.Substring(0, match.Groups[0].Index);
+            var innerText = text.Substring(match.Groups[1].Index, match.Groups[1].Length);
+            var postText = text.Substring(match.Groups[0].Index + match.Groups[0].Length);
+			var newText = preText;
+			if (keepInnerInfo)
+			{
+				newText = newText + "{" + tag + "}" + innerText + "{/" + tag + "}";
+
+            }
+            text = newText + postText;
+        }
+        while (match.Success);
+        return text;
+	}
+	
+	void OpenEbookFile(){
+		// Opening a book
+		//epubBook = EpubReader.OpenBook("austen-pride-and-prejudice-illustrations.epub");
+		//EpubChapter chapter = epubBook.Chapters [0];
+		//displayText.text = chapter.HtmlContent;
+		int standardFontSize = 10;
+		var epub = new UEPubReader("Assets/ebook/Books/austen-pride-and-prejudice-illustrations.epub");
+		var text = epub.chapters[10];
+        text = text.Replace("<br>", "\n");
+        text = text.Replace("<p>", "\n");
+        text = text.Replace("</p>", "\n");
+        var tagsToKeep = new List<string>()
+		{
+			//"p",
+			"head",
+			"b",
+			"em",
+			"h1",
+			"h2",
+			"h3",
+			"h4",
+			"h5",
+			"h6"
+		};
+		var tagsToDeleteInnerInfo = new List<string>() 
+		{ 
+			"title" 
+		};
+		foreach (var tag in tagsToKeep)
+		{
+			text = GetTextWithTags(text, tag, true);
+        }
+		foreach (var tag in tagsToDeleteInnerInfo)
+		{
+			text = GetTextWithTags(text, tag, false);
+		}
+        text = Regex.Replace(text, "<.*?>", String.Empty);
+		text = text.Replace("{head}", "<size=120%>");
+		text = text.Replace("{/head}", "</size>\n");
+		text = text.Replace("{title}", "<size=140%>");
+		text = text.Replace("{/title}", "</size>\n");
+		text = text.Replace("{b}", "<b>");
+		text = text.Replace("{/b}", "</b>");
+		text = text.Replace("{em}", "<em>");
+		text = text.Replace("{/em}", "</em>");
+		for (int hIterator = 1; hIterator <= 6; hIterator++)
+		{
+			var tag = "{h" + hIterator + "}";
+			var endTag = "{/h" + hIterator + "}";
+			int newSize = 100 + hIterator * 4;
+			text = text.Replace(tag, $"<size={newSize}>");
+			text = text.Replace(endTag, $"</size>\n");
+		}
+        /*var htmlStartTag = text.IndexOf("<html");
+		text = text.Replace("<em>", "{emphasis}");
+		text = text.Replace("</em>", "{emphasis/}");
+		text = text.Replace("<b>", "{bold}");
+		text = text.Replace("</b>", "{bold/}");*/
+        Debug.Log (epub.epubFolderLocation);
+		//displayText.text = "This is some <b><size=50><color=#ff0000ff>Text</color></size></b>";
+        displayText.text = text;
+		//File.WriteAllText("Assets/ebook/Books/austen-pride-and-prejudice-chapter-10.html", epub.chapters[10]);
+        //displayText.text = epub.chapters[10];
+    }
+}
---
+++ Assets/ebook/Scripts/UEPubReader.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System;
+using UnityEngine;
+
+namespace UEPub{
+	public class UEPubReader {
+
+		public string epubFolderLocation { get; private set; }
+		public string htmlRoot { get; private set; }
+		public UEPubMetadata metadata { get; private set; }
+
+		public List<string> chapters;
+
+		private Dictionary<string, string> bookItems;
+		private List<string> spine;
+
+		private string opfFileString;
+
+		public UEPubReader(string file){
+			bookItems = new Dictionary<string, string> ();
+			spine = new List<string> ();
+
+			var fileArray = file.Split('.');
+
+			if (fileArray [fileArray.Length - 1].ToLower() != "epub") {
+				Debug.LogErrorFormat ("The file {0} is not a .epub file", file);
+				return;
+			}
+
+			fileArray = string.Join (".", fileArray, 0, fileArray.Length - 1).Split (System.IO.Path.DirectorySeparatorChar);
+			var folderName = fileArray[fileArray.Length - 1];
+
+			epubFolderLocation = Application.temporaryCachePath + "/" + folderName;
+
+			ZipUtil.Unzip ( file, epubFolderLocation);
+
+			ParseContainer ();
+			ParseOPF ();
+		}
+
+		private void ParseContainer(){
+			var containerFile = epubFolderLocation + "/META-INF/container.xml";
+
+			XmlDocument doc = new XmlDocument ();
+			doc.Load (containerFile);
+
+			var xmlnsManager = new System.Xml.XmlNamespaceManager(doc.NameTable);
+			xmlnsManager.AddNamespace("ns", "urn:oasis:names:tc:opendocument:xmlns:container");
+
+			XmlNodeList rootFiles = doc.SelectNodes("/ns:container/ns:rootfiles/ns:rootfile", xmlnsManager);
+			opfFileString = rootFiles[0].Attributes ["full-path"].InnerText;
+		}
+
+		private void ParseOPF(){
+			var opfFile = epubFolderLocation + "/" + opfFileString;
+			
+			XmlDocument doc = new XmlDocument ();
+			doc.Load (opfFile);
+
+			var xmlnsManager = new System.Xml.XmlNamespaceManager(doc.NameTable);
+			xmlnsManager.AddNamespace("ns", "http://www.idpf.org/2007/opf");
+			xmlnsManager.AddNamespace("dc", "http://purl.org/dc/elements/1.1/");
+			xmlnsManager.AddNamespace("dcterms", "http://purl.org/dc/terms/");
+
+			ReadMetadata (doc, xmlnsManager);
+
+			ReadChapters (opfFile, doc, xmlnsManager);
+		}
+
+		private void ReadChapters (string opfFile, XmlDocument doc, XmlNamespaceManager xmlnsManager)
+		{
+			var nodes = doc.SelectNodes ("/ns:package/ns:manifest/ns:item", xmlnsManager);
+			foreach (XmlNode node in nodes) {
+				bookItems [node.Attributes ["id"].InnerText] = node.Attributes ["href"].InnerText;
+			}
+
+			nodes = doc.SelectNodes ("/ns:package/ns:spine/ns:itemref", xmlnsManager);
+			foreach (XmlNode node in nodes) {
+				spine.Add (node.Attributes ["idref"].InnerText);
+			}
+
+			var toc = spine.Select (x => bookItems [x]).ToList ();
+			chapters = new List<string> ();
+
+			htmlRoot = Path.GetDirectoryName (opfFile) + "/";
+
+			foreach (string s in toc) {
+				StreamReader sr = new StreamReader(htmlRoot + s);
+				chapters.Add (sr.ReadToEnd ());
+				sr.Close ();
+			}
+		}
+
+		private void ReadMetadata(XmlDocument doc, XmlNamespaceManager xmlnsManager){
+			metadata = new UEPubMetadata ();
+
+			// Required properties
+			metadata.title = doc.SelectSingleNode ("/ns:package/ns:metadata/dc:title", xmlnsManager).InnerText;
+			metadata.language = doc.SelectSingleNode ("/ns:package/ns:metadata/dc:language", xmlnsManager).InnerText;
+			metadata.identifier = doc.SelectSingleNode ("/ns:package/ns:metadata/dc:identifier", xmlnsManager).InnerText;
+
+			// Optional props
+			SetMetadataProperty (ref metadata.creator, "/ns:package/ns:metadata/dc:creator", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.contributor, "/ns:package/ns:metadata/dc:contributor", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.coverage, "/ns:package/ns:metadata/dc:coverage", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.description, "/ns:package/ns:metadata/dc:description", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.format, "/ns:package/ns:metadata/dc:format", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.publisher, "/ns:package/ns:metadata/dc:publisher", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.relation, "/ns:package/ns:metadata/dc:relation", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.rights, "/ns:package/ns:metadata/dc:rights", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.source, "/ns:package/ns:metadata/dc:source", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.subject, "/ns:package/ns:metadata/dc:subject", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.type, "/ns:package/ns:metadata/dc:type", doc, xmlnsManager);
+
+			// Date
+
+			var node = doc.SelectSingleNode ("/ns:package/ns:metadata/dc:date", xmlnsManager);
+
+			if (node != null) {
+				metadata.date = DateTime.ParseExact(node.InnerText, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+			}
+		}
+
+		private void SetMetadataProperty(ref string property, string XPath, XmlDocument doc, XmlNamespaceManager xmlnsManager){
+			var node = doc.SelectSingleNode (XPath, xmlnsManager);
+
+			if (node != null) {
+				property = node.InnerText;
+			}
+		}
+	}
+}
